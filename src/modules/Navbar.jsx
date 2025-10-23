84517eda2ee7eddf11a4fcfd69b0235793798acc
Kacsza <k.szadkowski2021@gmail.com>
2025-10-23 17:33:58 +0200

    dodanie komentarzy do Navbar.jsx

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -2,18 +2,23 @@ import { useState, useEffect, useRef } from "react";
 import './Navbar.css'
 
 function Navbar(){
+    // Zarządzanie stanem widoczności dropdownu
     const [isOpen, SetIsOpen] = useState(false);
     const toggleDropdown = () =>{
         SetIsOpen(!isOpen);
     }
+    // Stworzenie referencji do elementu dropdown w celu wykrywania kliknięć poza nim
     const DropdownRef = useRef(null);
 
     useEffect(()=>{
+        // Funkcja obsługująca kliknięcia poza dropdownem
         const HandleOutsideClicks = (event)=>{
             if(DropdownRef.current && !DropdownRef.current.contains(event.target)){
                 SetIsOpen(false);
             }
         };
+
+        // Dodanie nasłuchiwania zdarzenia kliknięcia na dokument
         document.addEventListener('mousedown', HandleOutsideClicks);
         return ()=>{
             document.removeEventListener('mousedown',HandleOutsideClicks);
