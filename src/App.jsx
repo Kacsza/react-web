870cfe5403cad8ee2f451d03c32656f7860ab481
Kacsza <k.szadkowski2021@gmail.com>
2025-10-23 21:06:57 +0200

    Dodano obiekt <Container>. Ma za zadanie ułatwić ustawianie obiektów strony. Coś czuje że to mnie prędzej czy później w dupe jebnie

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
 import Navbar from './modules/Navbar';
+import Container from './modules/Container';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Navbar />
+    <main>
+      <Container>
+        <h1>Nic tu ni ma</h1>
+        <p>Stronka będzie updatowana w miarę dalszej nauki.</p>
+        <p>Nawet nie wiem co tu dodać</p>
+      </Container>
+      <Container>
+        <h2>PS: Aleksio nie istnieje</h2>
+      </Container>
+    </main>
     </>
   )
 }
