b45434d7a8fec0bc9932ca9daa9e9dd90c439919
Kacsza <k.szadkowski2021@gmail.com>
2025-11-12 22:37:27 +0100

    React Router Działa!! Path /welcome przenosi do strony 404, tymczasowo. I tak pewnie mnie zajebie ale ciul

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,10 @@ function App() {
     <>
     <Navbar />
     <main>
-      <Container >
-        <h2>PS: Aleksio nie istnieje</h2>
-      </Container>
-      <Container className="span-2">
+      <Container>
         <h1>Nic tu ni ma</h1>
         <p>Stronka będzie updatowana w miarę dalszej nauki.</p>
         <p>Nawet nie wiem co tu dodać</p>  
-      </Container>
-      <Container>
-        <h1>Nowy kontener</h1>
-        <p>To jest nowy kontener dodany do siatki.</p>
       </Container>
     </main>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
+  {path: "/", element: <App />,},
+  {path:"*", element:<Navigate to="/" replace />},
+  {path:"/welcome", element:<div><h1>404 - page not found</h1></div>},
 ]);
 
 const root = document.getElementById("root");
