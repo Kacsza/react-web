67728d376847d2860ba34a343f14b7c26c146b76
Kacsza <k.szadkowski2021@gmail.com>
2025-11-22 20:44:12 +0100

    Dodano Tailwind CSS oraz jakoś działa. Narazie tylko Container.jsx korzysta

--- src/modules/Container.jsx
+++ src/modules/Container.jsx
@@ -1,10 +1,9 @@
 import './Container.css';
 
-const Container = ({ children, gridArea, className = '' }) => {
+const Container = ({ children }) => {
   return (
     <div 
-      className={`container ${className}`} 
-      style={gridArea ? { gridArea } : undefined}
+      className="relative bg-slate-600 flex flex-col items-center w-1/3 h-1/3 mx-auto my-12 p-6 rounded-2xl shadow-lg text-white justify-center"
     >
       {children}
     </div>
