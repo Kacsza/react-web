d4afb8092273d6794f41034408d9f61cc4b8757a
Kacsza <k.szadkowski2021@gmail.com>
2026-05-29 15:03:02 +0200

    instalation of react router, preparing for routing pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
-import { lazy, Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createHashRouter, Navigate, RouterProvider } from 'react-router';
-import App from './site/App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router/dom";
+import './index.css'
+import { router } from './App.jsx'
 
-// lazy load komponentów
-const Welcome = lazy(() => import('./site/Welcome.jsx'));
-
-const router = createHashRouter([
-  { path: "/", element: <App /> },
-  {
-    path: "/welcome",
-    // wrap lazy-loaded route component in Suspense
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Welcome />
-      </Suspense>
-    ),
-  },
-  { path: "*", element: <Navigate to="/" replace /> },
-]);
-
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
